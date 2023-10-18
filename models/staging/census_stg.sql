89bd8ca7b9d98eaa29a930ef1b60814d3f6b9ba8
the-tunnster <therealtarunkrishnan@gmail.com>
2023-10-18 18:00:58 +1100

    Added staging implementations for the data

---
+++ models/staging/census_stg.sql
@@ -0,0 +1,18 @@
+WITH table_1 AS (
+    SELECT * FROM {{ source('raw', 'census_data_g01') }}
+),
+table_2 AS (
+    SELECT * FROM {{ source('raw', 'census_data_g02') }}
+)
+
+SELECT table_1.*,
+    table_2.Median_mortgage_repay_monthly,
+	table_2.Median_age_persons,
+    table_2.Median_tot_prsnl_inc_weekly,
+    table_2.Median_rent_weekly,
+    table_2.Median_tot_fam_inc_weekly,
+    table_2.Average_num_psns_per_bedroom,
+    table_2.Median_tot_hhd_inc_weekly,
+    table_2.Average_household_size
+		FROM table_1
+			JOIN table_2 ON table_1.lga_code = table_2.lga_code
