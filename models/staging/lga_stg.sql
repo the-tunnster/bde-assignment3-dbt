15418d400fbdf7abef902a560bb9bbe51ce258b7
the-tunnster <therealtarunkrishnan@gmail.com>
2023-10-31 13:28:19 +1100

    I cant spell to save my life.

--- models/staging/lga_stg.sql
+++ models/staging/lga_stg.sql
@@ -8,8 +8,8 @@ table_2 AS (
 
 SELECT 
 	LOWER(table_1.LGA_NAME) as LGA_NAME,
-	LOWER(table_1.LGA_SUBURB) as LGA_SUBURB,
-	table_2.LGA_CODE as LGA_CODE
+	LOWER(table_2.LGA_SUBURB) as LGA_SUBURB,
+	table_1.LGA_CODE as LGA_CODE
 		FROM table_1
 			left JOIN table_2
 				ON LOWER(table_1.LGA_NAME) = LOWER(table_2.LGA_NAME)
